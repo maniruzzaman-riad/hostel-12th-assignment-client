afe59a1afdfeaa760d5aa1356e3fdfc88d248bae
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-12-06 12:48:18 +0600

    create admin route secure using jwt token

--- src/components/Hooks/useMealsSecure.jsx
+++ src/components/Hooks/useMealsSecure.jsx
@@ -4,6 +4,13 @@ export const axiosSecure = axios.create({
     baseURL: 'https://hostel-12th-assignment-server.vercel.app'
 })
 const useMealsSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token');
+        config.headers.authorization = `Bearer ${token}`
+        return config;
+    },function(error){
+        return Promise.reject(error)
+    })
     return axiosSecure;
 };
 
--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { app } from "../../firebase/firebase.config";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import useMealsPublic from "../Hooks/useMealsPublic";
 
 const provider = new GoogleAuthProvider()
 export const AuthContext = createContext(null);
 const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
+    const axiosPublic = useMealsPublic()
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -29,15 +31,28 @@ const AuthProvider = ({ children }) => {
         return signOut(auth);
     }
 
-    const userUpdate = (name,photo)=>{
-        return updateProfile(auth.currentUser,{
-            displayName:name,photoURL:photo
+    const userUpdate = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
         })
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+            if (currentUser) {
+                // to do :
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt',userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token',res.data.token)
+                    }
+                })
+            } else {
+                // to do something
+                localStorage.removeItem('access-token')
+            }
             setLoading(false)
         })
         return () => {
